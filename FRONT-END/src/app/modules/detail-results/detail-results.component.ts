7baa7d4ca7584500b374a3d3e50e070cee351df2
lopezcrony <lopezcrony@gmail.com>
2024-09-24 13:37:29 -0500

    Estoy que le atinoa ese link

---
+++ FRONT-END/src/app/modules/detail-results/detail-results.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-results.component.html',
+  styleUrl: './detail-results.component.css'
+})
+export class DetailResultsComponent {
+
+}
