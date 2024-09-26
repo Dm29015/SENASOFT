410984a52591bbb8f9438b076daa830103eda24b
Deisy Milena Correa Zabaleta <140520460+Dm29015@users.noreply.github.com>
2024-09-25 22:17:16 -0500

    Update gemini_ai.py

--- Back-IA/app/gemini_ai.py
+++ Back-IA/app/gemini_ai.py
@@ -1,5 +1,6 @@
 import google.generativeai as genai
 import markdown
+import textwrap
 
 GOOGLE_API_KEY = 'AIzaSyDX2celKEp4nGzuUi0cdXrk2bMktGWY7Fs'  # Asegúrate de usar tu clave API real
 
@@ -7,6 +8,12 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
+
+def to_markdown(text):
+    text = text.replace('•', '  *')
+    return markdown.markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
 def generate_prompt(prompt):
     response = model.generate_content(prompt)
-    return response.text
+    text_content = response.text
+    return to_markdown(text_content)
